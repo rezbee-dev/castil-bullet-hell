19cbb1dfaeca69f5846425ea8d3a07343462c2c1
devrisby <131298724+rezbee-dev@users.noreply.github.com>
2026-05-17 10:27:09 -0400

    changes sprites, add camera, and adjust lazers

--- scenes/lazer.gd
+++ scenes/lazer.gd
@@ -5,7 +5,7 @@ extends Node2D
 
 
 func _ready() -> void:
-	pass
+	hide()
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("LEFT_CLICK"):
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 100.0
 @export var wobble_speed: float = 5.0
 @export var wobble_intensity: float = 0.3
 @onready var sprite: Sprite2D = $Sprite
@@ -19,7 +19,5 @@ func _physics_process(delta: float) -> void:
 func _wobble():
 	if velocity.length() == 0:
 		sprite.rotation = lerp(sprite.rotation, 0.0, 0.2)
-		print("i'm not moving!!")
 	else:
 		sprite.rotation_degrees = rad_to_deg(sin(Time.get_ticks_msec() * 0.001 * wobble_speed) * wobble_intensity)
-		print("i'm wobbling!")
